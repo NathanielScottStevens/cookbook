ee85574c905a95a2f4568fd6d43bb19ff410b90c
Nathaniel Stevens <nathaniel.scott.stevens@gmail.com>
2016-10-08 19:48:49 -0400

    Moves Appbar and Navigation into App.

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,17 +1,76 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import withWidth, { LARGE } from 'material-ui/utils/withWidth';
+import Match from 'react-router/Match';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Router from 'react-router/BrowserRouter';
-import Match from 'react-router/Match';
 
+import AppBar from 'material-ui/AppBar';
+import Navigation from '../components/Navigation';
 import RecipeSelection from '../../ui/pages/RecipeSelection';
 
 
-const App = () => (
-  <MuiThemeProvider>
-    <Router>
-      <Match pattern="/" component={RecipeSelection} />
-    </Router>
-  </MuiThemeProvider>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = { isMenuOpen: false };
+  }
+
+  toggleMenu() {
+    this.setState({ isMenuOpen: !this.state.isMenuOpen });
+  }
+
+  render() {
+    let isMenuOpen = this.state.isMenuOpen;
+    let isMenuDocked = false;
+
+    if (this.props.width === LARGE) {
+      isMenuOpen = true;
+      isMenuDocked = true;
+    }
+
+    const styles = {
+      appBar: {
+        position: 'fixed',
+        top: 0,
+      },
+      root: {
+        paddingLeft: isMenuDocked ? 256 : 0,
+        paddingTop: 30,
+        margin: 50,
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+      },
+    };
+
+    return (
+      <MuiThemeProvider>
+        <Router>
+          <div>
+            <AppBar
+              title="Cookbook"
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+              onLeftIconButtonTouchTap={() => { this.toggleMenu(); }}
+              style={styles.appBar}
+            />
+            <Navigation
+              open={isMenuOpen}
+              docked={isMenuDocked}
+              onChange={() => { this.toggleMenu(); }}
+            />
+            <main style={styles.root}>
+              <Match pattern="/" component={RecipeSelection} />
+            </main>
+          </div>
+        </Router>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+App.propTypes = {
+  width: PropTypes.number,
+};
+
+export default withWidth()(App);
--- imports/ui/pages/RecipeSelection.jsx
+++ imports/ui/pages/RecipeSelection.jsx
@@ -1,77 +1,20 @@
-import React, { Component, PropTypes } from 'react';
-import withWidth, { LARGE } from 'material-ui/utils/withWidth';
+import React from 'react';
 import Match from 'react-router/Match';
 import Miss from 'react-router/Miss';
 
-import AppBar from 'material-ui/AppBar';
-import Navigation from '../components/Navigation';
 import RecipeCategoriesContainer from '../../ui/containers/RecipeCategories';
 import RecipeListContainer from '../../ui/containers/RecipeList';
 import RecipeContainer from '../../ui/containers/Recipe';
 import NotFound from '../../ui/pages/NotFound';
 
 
-class RecipeSelection extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { isMenuOpen: false };
-  }
-
-  toggleMenu() {
-    this.setState({ isMenuOpen: !this.state.isMenuOpen });
-  }
-
-  render() {
-    let isMenuOpen = this.state.isMenuOpen;
-    let isMenuDocked = false;
-
-    if (this.props.width === LARGE) {
-      isMenuOpen = true;
-      isMenuDocked = true;
-    }
-
-    const styles = {
-      appBar: {
-        position: 'fixed',
-        top: 0,
-      },
-      root: {
-        paddingLeft: isMenuDocked ? 256 : 0,
-        paddingTop: 30,
-        margin: 50,
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-      },
-    };
-
-    return (
-      <div>
-        <AppBar
-          title="Cookbook"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={() => { this.toggleMenu(); }}
-          style={styles.appBar}
-        />
-        <Navigation
-          open={isMenuOpen}
-          docked={isMenuDocked}
-          onChange={() => { this.toggleMenu(); }}
-        />
-        <main style={styles.root}>
-          <Match exactly pattern="/recipes" component={RecipeCategoriesContainer} />
-          <Match exactly pattern="/recipes/:id" component={RecipeListContainer} />
-          <Match exactly pattern="/recipes/:id/:id" component={RecipeContainer} />
-          <Miss component={NotFound} />
-        </main>
-      </div>
-    );
-  }
+export default function RecipeSelection() {
+  return (
+    <div>
+      <Match exactly pattern="/recipes" component={RecipeCategoriesContainer} />
+      <Match exactly pattern="/recipes/:id" component={RecipeListContainer} />
+      <Match exactly pattern="/recipes/:id/:id" component={RecipeContainer} />
+      <Miss component={NotFound} />
+    </div>
+  );
 }
-
-RecipeSelection.propTypes = {
-  width: PropTypes.number,
-};
-
-export default withWidth()(RecipeSelection);
